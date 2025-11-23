f48568bbc30f1eb951d53273ed0f14f3c5e298c1
Madhu Kalapala <Madhu.Kalapala@ibm.com>
2025-11-23 19:45:49 +0530

    Initial commit day 01

---
+++ day_01/data_types.py
@@ -0,0 +1,39 @@
+#Number (int, float, complex)
+#int 10, 20, 30 , 40 , 0 , -10
+#float 10.1, 102.3, -0.5, -10.5
+#complex 10-10j, 1+4j
+
+
+#String
+# madhu, 'madhu', "YES", 'NO'
+
+
+#Boolean
+#True 2+3==5, 3-1==2
+#False 2+3==6, 3-1=3
+
+
+#List
+#[1,20,10,15]
+#[1, 10.5, 34.5, 'madhu']
+
+
+#Set
+#{2,3,5,6,5,3,2,1} => {1,2,3,5,6}
+
+
+#Dictionary
+#{'a':'1', 'b:'2', 'c':'3'}
+
+
+#Tuple
+# (1,2,3)
+# ('madhu', 'babu', 'kalapala')
+
+a1,b1=2,3
+a2,b2=10,8
+a3 = (a2-a1)*(a2-a1)
+b3=(b2-b1)*(b2-b1)
+
+result=(a3+b3)
+print(result)
---
+++ day_01/helloworld.py
@@ -0,0 +1,24 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3**4)
+print(3//4)
+
+print("Madhu")
+print("Kalapala")
+print("India")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['madhu','python','india']))
+print(type('Madhu'))
+print(type("kalapala"))
+print(type("India"))
+
+
