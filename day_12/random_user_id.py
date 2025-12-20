b73bfd61216ba8a68b908ff08190692492829f90
Madhu Kalapala <Madhu.Kalapala@ibm.com>
2025-12-20 22:50:50 +0530

    Day 12 excerices

--- day_12/random_user_id.py
+++ day_12/random_user_id.py
@@ -11,3 +11,43 @@ def random_user_id(length=6):
     return id
 
 print(random_user_id())
+
+print("--------------------------------------")
+print("Taking input from user for length of id and how many ids")
+print("--------------------------------------")
+
+
+def user_id_gen_by_user():
+    id_length = int(input("Enter length of id: "))
+    no_of_ids = int(input("Enter number of ids to generate: "))
+    for j in range(no_of_ids):
+        print(random_user_id(id_length))
+
+user_id_gen_by_user()
+print('-----------------------------------------------------')
+
+def rgb_color_gen():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return f"rgb({r},{g},{b})"
+
+rgb_color = rgb_color_gen()
+print("Random RGB color: ", rgb_color)
+
+digits = string.digits
+def random_0_255():
+    while True:
+        value = ''.join(random.choices(digits, k=3))
+        if int(value) <= 255:
+            return value
+
+def rgb_color_gen_two_fun():
+
+    r = random_0_255()
+    g = random_0_255()
+    b = random_0_255()
+    return f"rgb({r},{g},{b})"
+rgb_color_two = rgb_color_gen_two_fun()
+print("Random RGB color using two functions: ", rgb_color_two)
+print('-----------------------------------------------------')
